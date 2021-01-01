Fixed sign-error in loss function. Also removed the "break if recent average change > 0" statement, because the corrected loss function changes more stochastically.

Module1/Exercise1/exercise_1.py
@@ -77,9 +77,9 @@ learning_rate = 0.001
 eta = 0.5  # Probability that sample is real and not noise
 nu = 1 / eta - 1
 batch_size = 100
-mask_type = "orthogonal"
+mask_type = "eight_neighbours"
 epochs = 3
-max_epoch = 100
+max_epoch = 500
 NCE = False
 tf_function = False
 saving = True
@@ -280,7 +280,7 @@ while True:
         if loss_diff[-1] > 0:
             learning_rate = learning_rate * decay_factor
             optimizer.lr.assign(learning_rate)
-            print(optimizer.get_config()["learning_rate"])
+            print("New learning rate: ", optimizer.get_config()["learning_rate"])
     if epoch >= 3:
         print(
             "Mean of last three loss diffs (",
@@ -289,8 +289,8 @@ while True:
             np.mean(loss_diff[-3:]),
             "\n",
         )
-        if np.mean(loss_diff[-3:]) > 0:
-            break
+        #if np.mean(loss_diff[-3:]) > 10000:
+        #    break
 
     if nan_precision:
         print("NaN precision!!")

Module1/Exercise1/exercise_1_model_and_functions.py
@@ -45,24 +45,20 @@ class GaussianModel(tf.keras.layers.Layer):
         ) ** 0.5
 
     def grad_log_model(self, data):
-        grad_x = -tf.einsum("ij,sj->si", self.precision_matrix, (data - self.loc))
+        grad_x = tf.einsum("ij,sj->si", self.precision_matrix, (data - self.loc))
         return grad_x
 
-    def laplace_log_model(self, data):
-        return -tf.einsum("ii", self.precision_matrix)
-
     def model_loss(self, data):
-        # 1/N sum i from 1 to N (1/2*||grad_x log model(xi)||^2 + laplace log model(xi))
+        # 1/N sum i from 1 to N (1/2*||grad_x log model(xi)||^2 + laplace log model
         N = len(data)
         loss = 0
         for sample in range(N):
             loss = (
                 loss
-                + 1 / 2 * tf.linalg.norm(self.grad_log_model(data[sample])) ** 2
-                + self.laplace_log_model(data[sample])
+                + tf.linalg.norm(self.grad_log_model(data[sample])) ** 2
             )
 
-        loss = 1 / N * loss
+        loss = 0.5 * (loss / N) - tf.einsum("ii", self.precision_matrix)
         return loss
 
     def data_point_weights(